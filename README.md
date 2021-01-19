f46fe8e2fc95e5fad5bbf897c610982964b23ff2
tuomashen <51713168+tuomashen@users.noreply.github.com>
2021-01-19 22:12:16 +0200

    db

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Link sharing application.
 
 https://thawing-savannah-46774.herokuapp.com
 
-Made with Vue.js and FastAPI.
+Made with Vue.js and FastAPI with Redis database.
--- users.py
+++ users.py
@@ -74,7 +74,7 @@ def user_info(uid: int):
 
 
 def get_newest():
-    r = redis.from_url(os.environ.get("REDIS_URL"))
+    r = redis.from_url(os.environ.get("REDIS_URL") + "?ssl_cert_reqs=None")
 
     new_users = r.lrange("newest", 0, -1)
 
